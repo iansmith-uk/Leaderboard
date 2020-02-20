ccc314f59f1acbba43980ba060c94402247a7ef6
Ian Smith <ian@iansmith.dev>
2020-02-20 20:37:07 +0000

    Added ability to remove players
    Added ability to remove players from leaderboard.
    
    Added Gold, Silver and Bronze colours for top 3 players

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { OutputLeaderboard } from "./Components/OutputLeaderboard";
 
 function App() {
   const [leaderboard, setLeaderBoard] = useState([]);
-  const updateLeaderboardArray = eachEntry => { setLeaderBoard([...leaderboard, eachEntry]);
+  const updateLeaderboardArray = eachEntry => { 
+    setLeaderBoard([...leaderboard, eachEntry]);
+    leaderboard.sort(function(a, b){
+      return a[2] - b[2];
+    }); //sorts by score
   };
 
   return (
     <div className="container mt-4">
       <InputForm updateLeaderboardArray={updateLeaderboardArray} />
-      <OutputLeaderboard leaderboard={leaderboard} />
+      <OutputLeaderboard setLeaderboard={setLeaderBoard} leaderboard={leaderboard} />
     </div>
   );
 }
--- src/Components/OutputLeaderboard.js
+++ src/Components/OutputLeaderboard.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Row, Col, Table } from 'reactstrap';
+import { Row, Col, Table, Button } from 'reactstrap';
+import './table.css';
+import { useRef } from 'react';
 
 export const OutputLeaderboard = props => {
-    const {leaderboard} = props;
+    const {leaderboard, setLeaderboard} = props;
   return (
     <div className="mt-4">
       <Row>
@@ -16,7 +18,7 @@ export const OutputLeaderboard = props => {
               </tr>
             </thead>
             <tbody>
-              <RenderTableData leaderboard={leaderboard} />
+              <RenderTableData setLeaderboard={setLeaderboard} leaderboard={leaderboard} />
             </tbody>
           </Table>
         </Col>
@@ -25,18 +27,45 @@ export const OutputLeaderboard = props => {
   );
 };
 const RenderTableData = props => {
-  const { leaderboard } = props;
+  const { leaderboard, setLeaderboard } = props;
   var count = 0;
   const finalArray = leaderboard.sort((a, b) => b.score - a.score);
+  const idRef = useRef();
+
+  const handleRemove = (id, e) => {
+    var currentBoard = leaderboard; //create copy of leaderboard
+    currentBoard.splice(id,1); //remove the score
+    setLeaderboard([...currentBoard]);//set the leaderboard in app.js
+    
+  }
+
   return Object.keys(finalArray).map((i, o) => {
     const { player, score } = finalArray[i];
     count = count + 1;
+
+    var htmlFor1st = "";
+    
+
+    if(count === 1){
+      htmlFor1st = 'first';
+    }
+    else if(count === 2){
+      htmlFor1st = 'second';
+    }
+    else if(count === 3){
+      htmlFor1st = 'third';
+    }
+
     return (
-      <tr key={count.toString(10)}>
-        <th scope="row">{count.toString(10)}</th>
+      <tr id={count.toString(10) - 1} key={count.toString(10)} className={htmlFor1st}>
+        <th scope="row" ref={idRef}>{count.toString(10)}</th>
         <td>{player}</td>
         <td>{score}</td>
+        <td><Button className="btn btn-error" onClick={(e) => handleRemove(count.toString(10) - 1)}>Delete</Button></td>
       </tr>
     );
+    
   });
+  
 };
+
